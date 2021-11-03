7448bd157256594a4994e4ad75094db031da4f44
Cameron Brilliant <cbrilliant20@gmail.com>
2021-11-03 08:56:15 -0400

    randomColor/chromaJS and text change on luminance

---
+++ app.js
@@ -0,0 +1,39 @@
+// Global Variables
+const colorDivs = document.querySelectorAll(".color")
+const generateButton = document.querySelector(".generate")
+const sliders = document.querySelectorAll('input[type="range"]')
+const currentHexes = document.querySelectorAll(".color h2")
+let initialColors
+
+// Functions
+
+function generateHex() {
+  // Chroma JS
+  const hexColor = chroma.random()
+  return hexColor
+}
+
+function randomColors() {
+  colorDivs.forEach((div, index) => {
+    const hexText = div.children[0]
+    const randomColor = generateHex()
+    // Add to background
+    div.style.backgroundColor = randomColor
+    hexText.innerText = randomColor
+    // Check for contrast
+    checkTextContrast(randomColor, hexText)
+  })
+}
+
+function checkTextContrast(color, text) {
+  const luminance = chroma(color).luminance()
+  if (luminance > 0.5) {
+    text.style.color = "black"
+  } else {
+    text.style.color = "white"
+  }
+}
+
+randomColors()
+
+// Event Listeners
