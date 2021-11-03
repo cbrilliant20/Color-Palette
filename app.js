235d4c46abb491cef57ea3b2405569cc91d19398
Cameron Brilliant <cbrilliant20@gmail.com>
2021-11-03 09:03:16 -0400

    setting variable sliders

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ function randomColors() {
     hexText.innerText = randomColor
     // Check for contrast
     checkTextContrast(randomColor, hexText)
+    // Initial Colorize sliders
+    const color = chroma(randomColor)
+    const sliders = div.querySelectorAll(".sliders input")
+    const hue = sliders[0]
+    const brightness = sliders[1]
+    const saturation = sliders[2]
+
+    colorizeSliders(color, hue, brightness, saturation)
   })
 }
 
@@ -32,6 +40,12 @@ function checkTextContrast(color, text) {
   } else {
     text.style.color = "white"
   }
+}
+
+function colorizeSliders(color, hue, brightness, saturation) {
+  // Saturation Scale
+  const noSat = color.set("hsl.s", 0)
+  const fullSat = color.set("hsl.s", 1)
 }
 
 randomColors()
