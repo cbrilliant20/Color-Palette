0c7d2155fbc8920a951d3e5e3f44c3862e5a6925
Cameron Brilliant <cbrilliant20@gmail.com>
2021-11-03 11:30:31 -0400

    dynamic color change

--- app.js
+++ app.js
@@ -14,9 +14,12 @@ function generateHex() {
 }
 
 function randomColors() {
+  initialColors = []
+
   colorDivs.forEach((div, index) => {
     const hexText = div.children[0]
     const randomColor = generateHex()
+    initialColors.push(chroma(randomColor).hex())
     // Add to background
     div.style.backgroundColor = randomColor
     hexText.innerText = randomColor
@@ -72,7 +75,8 @@ function hslControls(e) {
   const brightness = sliders[1]
   const saturation = sliders[2]
 
-  const bgColor = colorDivs[index].querySelector("h2").innerText
+  const bgColor = initialColors[index]
+
   let color = chroma(bgColor)
     .set("hsl.s", saturation.value)
     .set("hsl.l", brightness.value)
@@ -101,7 +105,6 @@ sliders.forEach((slider) => {
 })
 
 colorDivs.forEach((div, index) => {
-  console.log(div)
   div.addEventListener("change", () => {
     updateTextUI(index)
   })
