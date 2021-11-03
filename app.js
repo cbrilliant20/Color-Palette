beefdcf6198feda603b0f347b725a9a4e0de5554
Cameron Brilliant <cbrilliant20@gmail.com>
2021-11-03 09:31:41 -0400

    adjustments to sliders

--- app.js
+++ app.js
@@ -44,7 +44,6 @@ function checkTextContrast(color, text) {
 
 function colorizeSliders(color, hue, brightness, saturation) {
   // Hue Scale
-
   hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75), rgb(204,204,75), rgb(75,204,75), rgb(75,204,204), rgb(75,75,204), rgb(204,75,204), rgb(204,75,75))`
 
   // Brightness Scale
@@ -63,6 +62,29 @@ function colorizeSliders(color, hue, brightness, saturation) {
   )}, ${scaleSat(1)})`
 }
 
+function hslControls(e) {
+  const index =
+    e.target.getAttribute("data-bright") ||
+    e.target.getAttribute("data-sat") ||
+    e.target.getAttribute("data-hue")
+  let sliders = e.target.parentElement.querySelectorAll('input[type="range"]')
+  const hue = sliders[0]
+  const brightness = sliders[1]
+  const saturation = sliders[2]
+
+  const bgColor = colorDivs[index].querySelector("h2").innerText
+  let color = chroma(bgColor)
+    .set("hsl.s", saturation.value)
+    .set("hsl.l", brightness.value)
+    .set("hsl.h", hue.value)
+
+  colorDivs[index].style.backgroundColor = color
+}
+
 randomColors()
 
 // Event Listeners
+sliders.forEach((slider) => {
+  slider.addEventListener("input", hslControls)
+})
+
